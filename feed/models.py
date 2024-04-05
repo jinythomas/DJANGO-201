70c40d8c393499d40e89d16bafaa6f156d5a01b2
jinythomas <100972953+jinythomas@users.noreply.github.com>
2024-04-05 21:03:50 +0100

    Base file addition

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    text = models.CharField(max_length=240)
+    date = models.DateTimeField(auto_now=True) # auto_now makes the date field non editable
+
+    def __str__(self):
+        return self.text[0:100]
